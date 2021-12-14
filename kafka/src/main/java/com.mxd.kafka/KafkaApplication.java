e1303c6a34cdae055400f6ad7adc4bdc028c01d5
maxiaodong123 <aa52105210@163.com>
2021-12-14 17:53:33 +0800

    canal

--- kafka/src/main/java/com.mxd.kafka/KafkaApplication.java
+++ kafka/src/main/java/com.mxd.kafka/KafkaApplication.java
@@ -1,5 +1,6 @@
 package com.mxd.kafka;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -10,5 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaApplication {
 
-
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
 }
